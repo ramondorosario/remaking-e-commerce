2196d74c930c7d3ad474a3c54a39baf933d58931
ramondorosario <ramondorosario@gmail.com>
2021-04-22 00:38:31 -0300

    feat: putting stopwatch to work and removing banner when clicked

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useEffect, useState } from 'react';
 import { Money } from '../svg/Money';
 import { Ticket } from '../svg/Ticket';
 import { Time } from '../svg/Time';
 import s from './style.module.scss';
 
+let teste: NodeJS.Timeout;
+
 export const Banner: React.FC = () => {
+	const [time, setTime] = useState<number>(5 * 60);
+	const [bannerClicked, setBannerClicked] = useState<boolean>(false);
+
+	const minutes = Math.floor(time / 60)
+		.toString()
+		.padStart(2, '0');
+
+	const seconds = (time % 60).toString().padStart(2, '0');
+
+	useEffect(() => {
+		if (time !== 0 && !bannerClicked) {
+			teste = setTimeout(() => setTime(time - 1), 1000);
+		} else if (time === 0 || bannerClicked) {
+			clearTimeout(teste);
+		}
+	}, [time, bannerClicked]);
+
 	return (
-		<div className={s.container}>
+		<div
+			className={s.container}
+			onClick={() => setBannerClicked(true)}
+			hidden={bannerClicked || !time}
+		>
 			<div className={s.content}>
 				<div className={s.column}>
 					<h2>Aproveite agora</h2>
@@ -24,7 +48,9 @@ export const Banner: React.FC = () => {
 					<h2>Finaliza em</h2>
 					<div>
 						<Time />
-						<span>00:05:00</span>
+						<span>
+							00:{minutes}:{seconds}
+						</span>
 					</div>
 				</div>
 
