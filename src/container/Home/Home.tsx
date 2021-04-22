a75a999f32ca894685d446c99cb35f68004ec551
ramondorosario <ramondorosario@gmail.com>
2021-04-21 23:45:35 -0300

    feat: create home container

--- src/container/Home/Home.tsx
+++ src/container/Home/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Banner } from '../../components/Banner';
 import { Header } from '../../components/Header';
 
 import s from './style.module.scss';
@@ -7,6 +8,12 @@ export const Home: React.FC = () => {
 	return (
 		<div className={s.container}>
 			<Header />
+			<div className={s.content}>
+				<main>
+					<Banner />
+				</main>
+				<aside />
+			</div>
 		</div>
 	);
 };
