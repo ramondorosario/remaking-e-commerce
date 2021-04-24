4b8c3a82f61a38fa76b3bf600b98a9ed5cca79f6
ramondorosario <ramondorosario@gmail.com>
2021-04-23 21:34:11 -0300

    feat: creation of the cart wallet

--- src/container/Home/Home.tsx
+++ src/container/Home/Home.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Banner } from '../../components/Banner';
 import { Header } from '../../components/Header';
+import { Bag } from '../../components/svg/Bag';
+import { PersonIcon } from '../../components/svg/PersonIcon';
+import { Ticket } from '../../components/svg/Ticket';
 
 import s from './style.module.scss';
 
@@ -12,7 +15,24 @@ export const Home: React.FC = () => {
 				<main>
 					<Banner />
 				</main>
-				<aside />
+				<aside>
+					<div className={s.containerBag}>
+						<h2>
+							<Bag />
+							Sacola
+						</h2>
+						<div className={s.contentBag}>
+							<h3>Sua sacola esta vazia</h3>
+							<p>Adicione filmes agora</p>
+							<PersonIcon />
+							<p className={s.insertCupom}>Insira seu cupom</p>
+							<form>
+								<input type="text" placeholder="Cupom de desconto" />
+								<Ticket />
+							</form>
+						</div>
+					</div>
+				</aside>
 			</div>
 		</div>
 	);
