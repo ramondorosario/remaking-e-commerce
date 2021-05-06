273ca54591c13a1548c085611faa8652c559a02f
ramondorosario <ramondorosario@gmail.com>
2021-05-05 23:04:19 -0300

    feat: create movies list sections

--- src/container/Home/Home.tsx
+++ src/container/Home/Home.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Banner } from '../../components/Banner';
 import { Header } from '../../components/Header';
+import { MovieCard } from '../../components/MovieCard';
 import { Bag } from '../../components/svg/Bag';
 import { PersonIcon } from '../../components/svg/PersonIcon';
 import { Ticket } from '../../components/svg/Ticket';
+import { useMovies } from '../../contexts/MovieContext';
 
 import s from './style.module.scss';
 
 export const Home: React.FC = () => {
+	const {
+		getTop5Movies,
+		getPopularMovies,
+		movies,
+		top5PopularMovies,
+	} = useMovies();
+
+	useEffect(() => {
+		getTop5Movies();
+		getPopularMovies();
+	}, []);
+
 	return (
 		<div className={s.container}>
 			<Header />
 			<div className={s.content}>
 				<main>
 					<Banner />
+					<section className={s.containerMovies}>
+						<h2>Top Filmes</h2>
+						<div className={s.moviesList}>
+							{top5PopularMovies.map((movie) => {
+								return (
+									<MovieCard
+										key={movie.id}
+										name={movie.title}
+										price={movie.price}
+										rating={movie.rating}
+										url={movie.poster}
+									/>
+								);
+							})}
+						</div>
+					</section>
+					<section className={s.containerMovies}>
+						<h2>Filmes</h2>
+						<div className={s.moviesList}>
+							{movies.map((movie) => {
+								return (
+									<MovieCard
+										key={movie.id}
+										name={movie.title}
+										price={movie.price}
+										rating={movie.rating}
+										url={movie.poster}
+									/>
+								);
+							})}
+						</div>
+					</section>
 				</main>
 				<aside>
 					<div className={s.containerBag}>
-						<div className={s.headerBag}>
+						<header className={s.headerBag}>
 							<Bag />
 							<h2>Sacola</h2>
-						</div>
+						</header>
 						<div className={s.contentBag}>
 							<h3>Sua sacola esta vazia</h3>
 							<p>Adicione filmes agora</p>
