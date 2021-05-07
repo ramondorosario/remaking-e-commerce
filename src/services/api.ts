8b4ab87f5adbdb33ce48d1d2cdb331ec722f8e05
ramondorosario <ramondorosario@gmail.com>
2021-05-06 23:59:26 -0300

    chore: remove comment

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,4 +3,3 @@ import axios from 'axios';
 export const api = axios.create({
 	baseURL: 'https://tmdb-proxy-workers.vhfmag.workers.dev/3/',
 });
-// https://tmdb-proxy-workers.vhfmag.workers.dev/3/discover/movie?language=pt-BR
