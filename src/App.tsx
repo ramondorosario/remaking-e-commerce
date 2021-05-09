d02efc35be2c77b6ffe005180b6f2080c3994d8b
ramondorosario <ramondorosario@gmail.com>
2021-05-08 22:46:41 -0300

    feat: using ShopListProvider as wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Home } from './container/Home/Home';
 import './assets/styles/global.scss';
 import { MovieContextProvider } from './contexts/MovieContext';
+import { ShopListProvider } from './contexts/ShopListContext';
 
 export const App: React.FC = () => {
 	return (
 		<>
 			<MovieContextProvider>
-				<Home />
+				<ShopListProvider>
+					<Home />
+				</ShopListProvider>
 			</MovieContextProvider>
 		</>
 	);
