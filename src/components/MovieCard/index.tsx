f85a7631a4745f9436edeefd8ac7709f3b50c8b9
ramondorosario <ramondorosario@gmail.com>
2021-05-08 22:24:11 -0300

    feat: using shopList result as props and adding click event

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,38 +1,48 @@
 import React from 'react';
+import { useMovies } from '../../contexts/MovieContext';
+import { useShopList } from '../../contexts/ShopListContext';
 import { GoldStar } from '../svg/GoldStar';
 import { SilverStar } from '../svg/SilverStar';
 
 import s from './style.module.scss';
 
 interface IMovieCard {
-	name: string;
+	id: number;
+	title: string;
 	rating: number;
 	price: number;
-	url: string;
+	poster: string;
 }
 
-export const MovieCard: React.FC<IMovieCard> = ({
-	name,
-	rating,
-	price,
-	url,
-}: IMovieCard) => {
+export const MovieCard: React.FC<IMovieCard> = (movie: IMovieCard) => {
+	const { incrementMovieInTheShopList } = useShopList();
+	const { movies } = useMovies();
+
+	const movieItem = movies.find((item) => item.id === movie.id);
+
 	return (
 		<div className={s.container}>
-			<img src={url} alt="Banner do filme" />
+			<img src={movie.poster} alt="Banner do filme" />
 			<div className={s.content}>
 				<header>
 					<SilverStar />
 				</header>
 				<footer>
 					<div className={s.info}>
-						<span>{name}</span>
+						<span>{movie.title}</span>
 						<GoldStar />
-						<span>{rating}</span>
+						<span>{movie.rating}</span>
 					</div>
-					<button type="button">
+					<button
+						type="button"
+						onClick={() => {
+							if (movieItem) {
+								incrementMovieInTheShopList(movieItem);
+							}
+						}}
+					>
 						<span>Sacola</span>
-						<span>R$ {price.toString().replace('.', ',')}</span>
+						<span>R$ {movie.price.toString().replace('.', ',')}</span>
 					</button>
 				</footer>
 			</div>
