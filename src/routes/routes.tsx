6d07e1336a1a4b30c0e63efef1eb39723fe0c4df
ramondorosario <ramondorosario@gmail.com>
2021-05-10 23:11:06 -0300

    feat: rendering ConfirmData component when the route is "/confirmar-dados"

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,6 +5,7 @@ import {
 	Route,
 	Switch,
 } from 'react-router-dom';
+import { ConfirmData } from '../container/ConfirmData';
 import { Home } from '../container/Home/Home';
 
 export const Routes = () => {
@@ -13,7 +14,7 @@ export const Routes = () => {
 			<Switch>
 				<Route exact path="/" render={() => <Redirect to="/home" />} />
 				<Route path="/home" component={Home} />
-				<Route path="/confirmar-dados" component={Home} />
+				<Route path="/confirmar-dados" component={ConfirmData} />
 			</Switch>
 		</Router>
 	);
