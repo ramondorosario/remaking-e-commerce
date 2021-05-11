13320815ab2edfb10f50514a7b97c7eb3d23c96c
ramondorosario <ramondorosario@gmail.com>
2021-05-10 22:03:33 -0300

    feat: using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Home } from './container/Home/Home';
 import './assets/styles/global.scss';
 import { MovieContextProvider } from './contexts/MovieContext';
 import { ShopListProvider } from './contexts/ShopListContext';
+import { Routes } from './routes/routes';
 
 export const App: React.FC = () => {
 	return (
 		<>
 			<MovieContextProvider>
 				<ShopListProvider>
-					<Home />
+					<Routes />
 				</ShopListProvider>
 			</MovieContextProvider>
 		</>
---
+++ src/routes/routes.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+	BrowserRouter as Router,
+	Redirect,
+	Route,
+	Switch,
+} from 'react-router-dom';
+import { Home } from '../container/Home/Home';
+
+export const Routes = () => {
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/" render={() => <Redirect to="/home" />} />
+				<Route path="/home" component={Home} />
+				<Route path="/confirmar-dados" component={Home} />
+			</Switch>
+		</Router>
+	);
+};
