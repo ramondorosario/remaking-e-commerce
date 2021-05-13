e758f6c12091a7ab668df11fed26e2e4145692ae
ramondorosario <ramondorosario@gmail.com>
2021-05-12 23:54:23 -0300

    reafactor: using input component

--- src/container/ConfirmData/index.tsx
+++ src/container/ConfirmData/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
 import { ShopList } from '../../components/ShopList';
 import { PersonIcon } from '../../components/svg/PersonIcon';
 
@@ -14,68 +15,69 @@ export const ConfirmData: React.FC = () => {
 					<div>
 						<h2>Informações iniciais</h2>
 						<div className={s.containerInputs}>
-							<label>
-								<span>Insira seu nome completo</span>
-								<input type="text" placeholder="Ex: Ramon do Rosário" />
-							</label>
-							<label>
-								<span>Insira seu e-mail</span>
-								<input type="email" placeholder="Ex: exemplo@email.com" />
-							</label>
-							<label>
-								<span>Insira seu nome completo</span>
-								<input type="tel" placeholder="Ex: (99) 9 9999-9999" />
-							</label>
+							<Input
+								textLabel="Insira seu nome completo"
+								placeholder="Ex: Ramon Do Rosário"
+								type="text"
+							/>
+							<Input
+								textLabel="Insira seu e-mail"
+								placeholder="Ex: exemplo@email.com"
+								type="email"
+							/>
+							<Input
+								textLabel="Telefone"
+								placeholder="Ex: (99) 9 9999-9999"
+								type="tel"
+							/>
 						</div>
 					</div>
 					<div className={s.relative}>
 						<PersonIcon />
 						<h2>Informações de endereço</h2>
 						<div className={s.containerInputs}>
-							<label>
-								<span>CEP</span>
-								<input inputMode="numeric" placeholder="Ex: 00000-000" />
-							</label>
-							<label>
-								<span>País</span>
-								<input type="text" placeholder="Ex: Brasil" />
-							</label>
-							<label>
-								<span>Cidade</span>
-								<input type="text" placeholder="Ex: Salvador" />
-							</label>
-							<label>
-								<span>Bairro</span>
-								<input type="text" placeholder="Ex: São Rafael" />
-							</label>
-							<label>
-								<span>Número</span>
-								<input inputMode="numeric" placeholder="Ex: 00" />
-							</label>
+							<Input
+								textLabel="CEP"
+								placeholder="Ex: 00000-000"
+								type="number"
+							/>
+							<Input textLabel="País" placeholder="Ex: Brasil" type="text" />
+							<Input
+								textLabel="Cidade"
+								placeholder="Ex: Salvador"
+								type="text"
+							/>
+							<Input
+								textLabel="Bairro"
+								placeholder="Ex: São Rafael"
+								type="text"
+							/>
+							<Input
+								textLabel="Número"
+								placeholder="Ex: São Rafael"
+								type="number"
+							/>
 						</div>
 					</div>
 					<div>
 						<h2>Informações de pagamento</h2>
 						<div className={s.containerInputs}>
-							<label>
-								<span>Número do cartão</span>
-								<input
-									inputMode="numeric"
-									placeholder="Ex: 0000.0000.0000.0000"
-								/>
-							</label>
-							<label>
-								<span>Nome do cartão</span>
-								<input type="text" placeholder="Ex: Fulano de Tal" />
-							</label>
-							<label>
-								<span>Data de expiração</span>
-								<input type="text" placeholder="MM/YYYY" />
-							</label>
-							<label>
-								<span>CVV</span>
-								<input type="text" placeholder="Ex: São Rafael" />
-							</label>
+							<Input
+								textLabel="Número do cartão"
+								placeholder="Ex: 0000.0000.0000.0000"
+								type="number"
+							/>
+							<Input
+								textLabel="Nome do cartão"
+								placeholder="Ex: Fulano de Tal"
+								type="text"
+							/>
+							<Input
+								textLabel="Data de expiração"
+								placeholder="Ex: MM/YYYY"
+								type="number"
+							/>
+							<Input textLabel="CVV" placeholder="Ex: 000" type="number" />
 						</div>
 					</div>
 				</form>
