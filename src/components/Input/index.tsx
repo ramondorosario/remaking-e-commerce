9b9e227c01f544418f43152ed120f8e43a2bc0c3
ramondorosario <ramondorosario@gmail.com>
2021-05-15 17:57:29 -0300

    feat: adding new props

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,20 +1,23 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/require-default-props */
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, ReactNode } from 'react';
 
 import s from './style.module.scss';
 
 interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
 	textLabel: string;
+	elementError: ReactNode;
+	register: any;
 }
 
 export const Input: React.FC<IInputProps> = (props) => {
-	const { textLabel } = props;
+	const { textLabel, elementError, className, register } = props;
 
 	return (
-		<label>
+		<label className={`${s.container} ${className}`}>
 			<span>{textLabel}</span>
-			<input {...props} />
+			<input {...register} {...props} />
+			{elementError ?? ''}
 		</label>
 	);
 };
